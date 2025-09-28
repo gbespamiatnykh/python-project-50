31813d3880a1f87a98ad8d2f4cdd834da126690f
George Bespamiatnykh <george.bespamiatnykh@gmail.com>
2025-09-28 12:09:00 +0700

    add functions to gendiff.py and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/gbespamiatnykh/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/gbespamiatnykh/python-project-50/actions)
+
+
+### generate-diff
+Run Command:
+```bash
+gendiff
+```
+<details>
+<summary>generate-diff demo</summary>
+
+[![asciicast](https://asciinema.org/a/3Sc4v8rbAECEUtu2PTqoO1EIj.svg)](https://asciinema.org/a/3Sc4v8rbAECEUtu2PTqoO1EIj)
+</details>
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,39 @@
 import argparse
 import json
+from typing import Any
 
 
-def main():
+def bool_to_lower_str(value) -> str | Any:
+    if isinstance(value, bool):
+        return str(value).lower()
+    return value
+
+
+def generate_diff(first_file: dict, second_file: dict) -> str:
+    difference = ["{", "}"]
+    for key in sorted(set(first_file) | set(second_file)):
+        if key in first_file and key not in second_file:
+            difference.insert(
+                -1, f"  - {key}: {bool_to_lower_str(first_file[key])}"
+                )
+        elif key not in first_file and key in second_file:
+            difference.insert(
+                -1, f"  + {key}: {bool_to_lower_str(second_file[key])}"
+                )
+        elif first_file[key] != second_file[key]:
+            difference.insert(
+                -1,
+                f"  - {key}: {bool_to_lower_str(first_file.get(key))}\n"
+                f"  + {key}: {bool_to_lower_str(second_file[key])}"
+            )
+        else:
+            difference.insert(
+                -1, f"    {key}: {bool_to_lower_str(first_file[key])}"
+                )
+    return "\n".join(difference)
+
+
+def main() -> None:
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows a difference."
     )
@@ -12,7 +43,7 @@ def main():
     args = parser.parse_args()
     first_file = json.load(open(args.first_file))
     second_file = json.load(open(args.second_file))
-    print(f"first_file: {first_file}\nsecond_file: {second_file}")
+    print(generate_diff(first_file, second_file))
 
 
 if __name__ == "__main__":
