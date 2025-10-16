80ab37beb63c94538d65d47af88f37d2d4b83a5c
George Bespamiatnykh <george.bespamiatnykh@gmail.com>
2025-10-17 05:08:15 +0700

    update json.py and fix gendiff.py

--- gendiff/formatter/json.py
+++ gendiff/formatter/json.py
@@ -1,5 +1,6 @@
 from json import dumps
+from typing import Any
 
 
-def format_json(diff):
+def format_json(diff: list[dict[str, Any]]) -> str:
     return dumps(diff, indent=4)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,7 +7,9 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows a difference."
     )
-    parser.add_argument("-f", "--format", help="set format of output")
+    parser.add_argument(
+        "-f", "--format", help="set format of output", default="stylish"
+    )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     return parser.parse_args()
