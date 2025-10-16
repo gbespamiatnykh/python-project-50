ac2a46452178b685299af6a889b6e7a3aa9a3fb4
George Bespamiatnykh <george.bespamiatnykh@gmail.com>
2025-10-17 04:48:23 +0700

    add json.py and update formatter.py

--- gendiff/formatter/formatter.py
+++ gendiff/formatter/formatter.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from gendiff.formatter.json import format_json
 from gendiff.formatter.plain import format_plain
 from gendiff.formatter.stylish import format_stylish
 
@@ -10,3 +11,5 @@ def format_diff(diff: list[dict[str, Any]], format_name: str) -> str:
             return format_stylish(diff)
         case "plain":
             return format_plain(diff)
+        case "json":
+            return format_json(diff)
